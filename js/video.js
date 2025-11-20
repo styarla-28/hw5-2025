4d02eea01d596142a3a65367d5d683960a00a277
Shrina Tyarla <styarla@umich.edu>
2025-11-19 23:29:53 -0500

    first commit

--- js/video.js
+++ js/video.js
@@ -1,11 +1,65 @@
-var video;
+let vid = document.getElementById("player1");
+
 
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
 
 });
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	vid.play()
+});
+
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	vid.pause();
+});
+
+document.querySelector("#slower").addEventListener("click", function() {
+	vid.playbackRate = vid.playbackRate*0.9;
+	console.log("Video is playing back at:", vid.playbackRate.toFixed(2));
+});
+
+
+document.querySelector("#faster").addEventListener("click", function() {
+	vid.playbackRate = vid.playbackRate*1.1;
+	console.log("Video is playing back at:", vid.playbackRate.toFixed(2));
+});
+
+document.querySelector("#skip").addEventListener("click", function(){
+	const newTime = vid.currentTime + 10;
+	if (newTime >= vid.duration){
+		vid.currentTime = 0;
+		console.log("Video reset to the start");
+	}
+	else{
+		vid.currentTime = newTime;
+		console.log("Video advanced. Current time in seconds:", vid.currentTime.toFixed(2))
+	}
+});
+
+document.querySelector("#mute").addEventListener("click", function (){
+	vid.muted = !vid.muted;
+	if (vid.muted){
+		document.getElementById("mute").textContent = "Unmute";
+	}
+	else{
+		document.getElementById("mute").textContent = "Mute";
+	}
+});
+
+// vidVolume.textContent = '${Math.round(volumeSlider*100)}%';
+
+// document.querySelector("#slider").addEventListener("input", function(){
+// 	let volumeSlider = document.getElementById("slider");
+// 	let vidVolume = document.getElementById("volume");
+
+// 	vid.volume = volumeSlider.value;
+// 	vidVolume.textContent = '${Math.round(vid.volume*100)}%';
 // });
+
+
+
+
 
